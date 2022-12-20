5bba17d807d11b33362f148a270443229e118b46
drumath2237 <2237k.drumath@gmail.com>
2022-12-20 21:42:53 +0900

    :sparkles: kinectのフレームを非同期ストリームで取得する拡張メソッドの実装

---
+++ Assets/Scripts/DeviceExtension.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Kinect.Sensor;
+
+namespace AzureKinectAsyncStreamSandbox
+{
+    public static partial class DeviceExtension
+    {
+        public static async IAsyncEnumerable<Capture> GetAsyncFrameStream(
+            this Device device,
+            [EnumeratorCancellation] CancellationToken token
+        )
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var capture = await Task.Run(device.GetCapture, token);
+                yield return capture;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/KinectTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Kinect.Sensor;
+using UnityEngine;
+
+namespace AzureKinectAsyncStreamSandbox
+{
+    public class KinectTest : MonoBehaviour
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+        private SynchronizationContext _synchronizationContext;
+        [SerializeField] private Texture2D colorTexture;
+
+        private void Start()
+        {
+            _synchronizationContext = SynchronizationContext.Current;
+            _ = StartCaptureLoop();
+        }
+
+        private async Task StartCaptureLoop()
+        {
+            using var kinect = Device.Open();
+            kinect.StartCameras(new DeviceConfiguration
+            {
+                ColorFormat = ImageFormat.ColorMJPG,
+                ColorResolution = ColorResolution.R720p,
+                DepthMode = DepthMode.Off,
+                SynchronizedImagesOnly = false,
+                CameraFPS = FPS.FPS30,
+            });
+
+            await foreach (var capture in kinect.GetAsyncFrameStream(_cancellationTokenSource.Token))
+            {
+                var jpegData = capture.Color.Memory.ToArray();
+                _synchronizationContext.Post(_ =>
+                {
+                    if (colorTexture == null)
+                    {
+                        return;
+                    }
+
+                    colorTexture.LoadRawTextureData(jpegData);
+                    colorTexture.Apply();
+                }, null);
+                capture.Dispose();
+            }
+            
+            kinect.StopCameras();
+        }
+
+        private void OnDestroy()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
